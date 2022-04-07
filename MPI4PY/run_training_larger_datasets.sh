4c552c67bd2bd34debbead4cde449bce642b0085
Yong-gui <68081216+Yong-gui@users.noreply.github.com>
2022-04-07 16:13:16 -0400

    Update run_training_larger_datasets.sh

--- MPI4PY/run_training_larger_datasets.sh
+++ MPI4PY/run_training_larger_datasets.sh
@@ -9,12 +9,7 @@ echo
 ###################################################################################################
 echo
 echo "Training Resnet18 on the CINIC10 dataset"
-
-echo "propresses the CINIC10 dataset"
-cd data/CINIC-10
-./preprocess_CINIC10.sh
-cd ../..
-
+ 
 data_name=CINIC-10
 model_name=resnet18
 num_classes=10
@@ -32,8 +27,6 @@ mpirun -np 2  python $function_name --data_name=$data_name --model_name=$model_n
 mpirun -np 6  python $function_name --data_name=$data_name --model_name=$model_name --num_classes=$num_classes --epochs=$epochs --alpha=$alpha --train_batch_size=$train_batch_size --opt_name=apam --communication=sync --cuda=False
 mpirun -np 11 python $function_name --data_name=$data_name --model_name=$model_name --num_classes=$num_classes --epochs=$epochs --alpha=$alpha --train_batch_size=$train_batch_size --opt_name=apam --communication=sync --cuda=False
 mpirun -np 21 python $function_name --data_name=$data_name --model_name=$model_name --num_classes=$num_classes --epochs=$epochs --alpha=$alpha --train_batch_size=$train_batch_size --opt_name=apam --communication=sync --cuda=False
-echo "plot the running time with CPUs by APAM and sync AMSGrad"
-python plot_CINIC_async_sync_cpu_time_compare.py --epochs=$epochs
 ###################################################################################################
 echo "running APAM with GPUs to see the effect of the delay."
 epochs=200
@@ -42,9 +35,6 @@ mpirun -np 2  python $function_name --data_name=$data_name --model_name=$model_n
 mpirun -np 3  python $function_name --data_name=$data_name --model_name=$model_name --num_classes=$num_classes --epochs=$epochs --alpha=$alpha --train_batch_size=$train_batch_size --opt_name=apam --communication=async --cuda=True --log_per_epoch=$log_per_epoch
 mpirun -np 6  python $function_name --data_name=$data_name --model_name=$model_name --num_classes=$num_classes --epochs=$epochs --alpha=$alpha --train_batch_size=$train_batch_size --opt_name=apam --communication=async --cuda=True --log_per_epoch=$log_per_epoch
 mpirun -np 11 python $function_name --data_name=$data_name --model_name=$model_name --num_classes=$num_classes --epochs=$epochs --alpha=$alpha --train_batch_size=$train_batch_size --opt_name=apam --communication=async --cuda=True --log_per_epoch=$log_per_epoch
-echo "plot the prediction accuracy by APAM with GPUs"
-python plot_CINIC_async_gpu_different_workers.py --epochs=$epochs --log_per_epoch=$log_per_epoch
-echo
 echo
 
 ###################################################################################################
@@ -53,11 +43,6 @@ echo
 ####  Model: WRN-28-5(wideresnet28_5)
 ###################################################################################################
 echo "Training WRN-28-5 on the imagenet32*32 dataset"
-
-echo "propresses the imagenet32*32 dataset"
-cd data/imagenet32
-preprocess_imagenet32.sh
-cd ../..
 
 data_name=imagenet32
 model_name=wideresnet28_5
@@ -76,8 +61,6 @@ mpirun -np 2  python $function_name --data_name=$data_name --model_name=$model_n
 mpirun -np 6  python $function_name --data_name=$data_name --model_name=$model_name --num_classes=$num_classes --epochs=$epochs --alpha=$alpha --train_batch_size=$train_batch_size --opt_name=apam --communication=sync --cuda=False
 mpirun -np 11 python $function_name --data_name=$data_name --model_name=$model_name --num_classes=$num_classes --epochs=$epochs --alpha=$alpha --train_batch_size=$train_batch_size --opt_name=apam --communication=sync --cuda=False
 mpirun -np 21 python $function_name --data_name=$data_name --model_name=$model_name --num_classes=$num_classes --epochs=$epochs --alpha=$alpha --train_batch_size=$train_batch_size --opt_name=apam --communication=sync --cuda=False
-echo "plot the running time with CPUs by APAM and sync AMSGrad"
-python plot_imagenet32_async_sync_cpu_time_compare.py --epochs=$epochs
 ###################################################################################################
 echo "running APAM with GPUs to see the effect of the delay."
 epochs=40
@@ -86,7 +69,4 @@ mpirun -np 2  python $function_name --data_name=$data_name --model_name=$model_n
 mpirun -np 3  python $function_name --data_name=$data_name --model_name=$model_name --num_classes=$num_classes --epochs=$epochs --alpha=$alpha --train_batch_size=$train_batch_size --opt_name=apam --communication=async --cuda=True --log_per_epoch=$log_per_epoch
 mpirun -np 6  python $function_name --data_name=$data_name --model_name=$model_name --num_classes=$num_classes --epochs=$epochs --alpha=$alpha --train_batch_size=$train_batch_size --opt_name=apam --communication=async --cuda=True --log_per_epoch=$log_per_epoch
 mpirun -np 11 python $function_name --data_name=$data_name --model_name=$model_name --num_classes=$num_classes --epochs=$epochs --alpha=$alpha --train_batch_size=$train_batch_size --opt_name=apam --communication=async --cuda=True --log_per_epoch=$log_per_epoch
-echo "plot the prediction accuracy by APAM with GPUs"
-python plot_imagenet32_async_gpu_different_workers.py --epochs=$epochs --log_per_epoch=$log_per_epoch
-echo
 echo
